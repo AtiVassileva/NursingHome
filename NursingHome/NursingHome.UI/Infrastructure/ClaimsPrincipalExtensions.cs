9fdf40ac0f36a239aac653adc8ca33c14c8e1bc4
Ati <ativassileva@gmail.com>
2025-05-02 22:42:01 +0300

    Added pages stylization

---
+++ NursingHome/NursingHome.UI/Infrastructure/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Identity;
+using NursingHome.DAL.Models;
+
+namespace NursingHome.UI.Infrastructure
+{
+    using static Common.WebConstants;
+    public static class ClaimsPrincipalExtensions
+    {
+        public static string GetId(this ClaimsPrincipal user)
+            => user.FindFirst(ClaimTypes.NameIdentifier)!.Value;
+
+        public static bool IsAdmin(this ClaimsPrincipal user)
+            => user.IsInRole(AdministratorRoleName);
+
+        public static bool IsEmployee(this ClaimsPrincipal user)
+            => user.IsInRole(EmployeeRoleName);
+
+        public static bool IsResident(this ClaimsPrincipal user)
+            => user.IsInRole(RegularUserRoleName);
+
+        public static string GetFullName(this ClaimsPrincipal user, UserManager<ApplicationUser> userManager)
+        {
+            var fullName = string.Empty;
+
+            Task.Run(async () =>
+                {
+                    var userDetails = await userManager.GetUserAsync(user);
+
+                    if (userDetails != null)
+                    {
+                        fullName = string.Concat(userDetails.FirstName, " ", userDetails.LastName);
+                    }
+                })
+                .GetAwaiter()
+                .GetResult();
+
+            return fullName;
+        }
+    }
+}
